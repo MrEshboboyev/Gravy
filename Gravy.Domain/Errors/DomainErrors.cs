eccb5ae261e125c9531f3d89783cd1dbc6e68d70
MrEshboboyev <mreshboboyev@gmail.com>
2024-11-24 20:10:13 +0500

    Default Domain Errors in Domain/Errors

---
+++ Gravy.Domain/Errors/DomainErrors.cs
@@ -0,0 +1,15 @@
+﻿namespace Gravy.Domain.Errors;
+
+public static class DomainErrors
+{
+    // entities errors with static readonly
+    // Example
+
+    //public static class Member
+    //{
+    //    public static readonly Error EmailAlreadyInUse = new(
+    //        "Member.EmailAlreadyInUse",
+    //        "The specified email is already in use");
+    //}
+}
+
