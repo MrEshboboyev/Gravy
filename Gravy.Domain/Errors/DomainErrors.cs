90f47a94e0ea2cbad5495cc46605a0040f03776d
MrEshboboyev <mreshboboyev@gmail.com>
2024-11-24 20:43:46 +0500

    IAuditableEntity with created and modified fields, DomainErrors explained with summary

--- Gravy.Domain/Errors/DomainErrors.cs
+++ Gravy.Domain/Errors/DomainErrors.cs
@@ -1,12 +1,16 @@
 ﻿namespace Gravy.Domain.Errors;
 
+/// <summary> 
+/// Defines and organizes domain-specific errors. 
+/// </summary>
 public static class DomainErrors
 {
-    // entities errors with static readonly
-    // Example
-
+    //// Example of defining errors for the Member entity
     //public static class Member
     //{
+    //    /// <summary> 
+    //    /// Error indicating that the specified email is already in use. 
+    //    /// </summary> 
     //    public static readonly Error EmailAlreadyInUse = new(
     //        "Member.EmailAlreadyInUse",
     //        "The specified email is already in use");
